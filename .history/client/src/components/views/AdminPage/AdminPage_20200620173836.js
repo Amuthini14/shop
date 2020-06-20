2c79d918ab7055951c52b413aa3edef5747c7dc5
Amouthinie <akulatheepan@gmail.com>
2020-06-20 18:14:34 +0530

    heroku deployment

---
+++ .history/client/src/components/views/AdminPage/AdminPage_20200620173836.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdminPage() {
+    return (
+        <div>
+            <h1>admin page</p>
+        </div>
+    )
+}
+
+export default AdminPage
---
+++ client/build/precache-manifest.cfe71f3d8d9d2bdaf29b7f17c7108b5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb20d2219b0725869b81fc76c2de61c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f87b44a7aa590751f764",
+    "url": "/static/css/2.54c7b650.chunk.css"
+  },
+  {
+    "revision": "c79b2736ffcf49d13daf",
+    "url": "/static/css/main.275b9e42.chunk.css"
+  },
+  {
+    "revision": "f87b44a7aa590751f764",
+    "url": "/static/js/2.a3b91f50.chunk.js"
+  },
+  {
+    "revision": "77fef8d96be745599e4b18d420929573",
+    "url": "/static/js/2.a3b91f50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c79b2736ffcf49d13daf",
+    "url": "/static/js/main.39c7b4ce.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
